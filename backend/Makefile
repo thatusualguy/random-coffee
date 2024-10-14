43b144a0d6c4254972c78405ad93503d58fd939d
Qquiqlerr <leha.metlusko@bk.ru>
2024-10-14 14:02:27 +0300

    Надеюсь я ничего не сломал

---
+++ backend/Makefile
@@ -0,0 +1,5 @@
+auth_proto:
+	protoc --go_out=. --go-grpc_out=. ./api/proto/auth/auth.proto
+
+user_proto:
+	protoc --go_out=. --go-grpc_out=. ./api/proto/user/user.proto
---
+++ backend/auth/internal/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"github.com/dotenv-org/godotenvvault"
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	AccessTokenTTL  time.Duration `yaml:"access_token_ttl"`
+	RefreshTokenTTL time.Duration `yaml:"refresh_token_ttl"`
+	GRPC            `yaml:"grpc"`
+	Secret          string
+}
+
+type GRPC struct {
+	Port    string        `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoadConfig() *Config {
+	err := godotenvvault.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	path := os.Getenv("CONFIG_PATH")
+	if path == "" {
+		panic("empty config path")
+	}
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config is not exist")
+	}
+	var cfg Config
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("failed to parse config")
+	}
+	cfg.Secret = os.Getenv("SECRET")
+	return &cfg
+}
