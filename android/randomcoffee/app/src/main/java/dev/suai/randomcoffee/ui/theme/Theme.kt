d6f1ef840ca1d136879ecda144096a1dbf2fc4aa
Maksim Rybakov <m.p.rybakov@gmail.com>
2024-10-16 21:23:03 +0300

    use color theme

--- android/randomcoffee/app/src/main/java/dev/suai/randomcoffee/ui/theme/Theme.kt
+++ android/randomcoffee/app/src/main/java/dev/suai/randomcoffee/ui/theme/Theme.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -12,15 +13,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    background = Brown,
+    primary = BrownLight,
+    secondary = Orange,
+    tertiary = OrangeLight,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    background = OrangeLight,
+    primary = Orange,
+    secondary = Brown,
+    tertiary = BrownLight,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -36,16 +39,9 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun RandomCoffeeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
@@ -54,6 +50,10 @@ fun RandomCoffeeTheme(
         colorScheme = colorScheme,
         typography = Typography,
         shapes = Shapes,
-        content = content
+        content = {
+            Surface(color = colorScheme.background) {
+                content.invoke()
+            }
+        }
     )
 }
