ac68d68431c0887cd4ab5de0c3209b5f7fe6f634
Qquiqlerr <leha.metlushko@bk.ru>
2024-10-25 22:03:17 +0300

    add migrations

--- user/cmd/server/main.go
+++ user/cmd/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"github.com/pressly/goose/v3"
 	"service.user/internal/repository"
 	"service.user/pkg/config"
 	"service.user/pkg/logger"
@@ -11,5 +13,6 @@ func main() {
 	cfg := config.MustLoad()
 	pool := storage.MustConnect()
 	log := logger.SetupLogger(cfg.Env)
+	goose.Up(pool.(*sql.DB))
 	repo := repository.NewRepository(pool, log)
 }
