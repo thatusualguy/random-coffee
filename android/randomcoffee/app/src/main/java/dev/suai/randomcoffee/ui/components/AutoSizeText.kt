d3509569279f1cea8450b8231b236d66cfb03b09
Maksim Rybakov <m.p.rybakov@gmail.com>
2024-11-18 06:21:00 +0300

    FEATURE: add alternative for AutoSizeText

--- android/randomcoffee/app/src/main/java/dev/suai/randomcoffee/ui/components/AutoSizeText.kt
+++ android/randomcoffee/app/src/main/java/dev/suai/randomcoffee/ui/components/AutoSizeText.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -21,6 +22,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 
 sealed class AutoSizeConstraint(open val min: TextUnit = TextUnit.Unspecified) {
     data class Width(override val min: TextUnit = TextUnit.Unspecified) : AutoSizeConstraint(min)
@@ -134,4 +136,81 @@ fun AutoSizeText(
         style = style,
         constraint = constraint
     )
+}
+
+
+
+@Composable
+fun AutoResizeText(
+    text: String,
+    fontSizeRange: FontSizeRange,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    style: TextStyle = LocalTextStyle.current,
+) {
+    var fontSizeValue by remember { mutableFloatStateOf(fontSizeRange.max.value) }
+    var readyToDraw by remember { mutableStateOf(false) }
+
+    Text(
+        text = text,
+        color = color,
+        maxLines = maxLines,
+        fontStyle = fontStyle,
+        fontWeight = fontWeight,
+        fontFamily = fontFamily,
+        letterSpacing = letterSpacing,
+        textDecoration = textDecoration,
+        textAlign = textAlign,
+        lineHeight = lineHeight,
+        overflow = overflow,
+        softWrap = softWrap,
+        style = style,
+        fontSize = fontSizeValue.sp,
+        onTextLayout = {
+            if (it.didOverflowHeight && !readyToDraw) {
+                val nextFontSizeValue = fontSizeValue - fontSizeRange.step.value
+                if (nextFontSizeValue <= fontSizeRange.min.value) {
+                    // Reached minimum, set minimum font size and it's readToDraw
+                    fontSizeValue = fontSizeRange.min.value
+                    readyToDraw = true
+                } else {
+                    // Text doesn't fit yet and haven't reached minimum text range, keep decreasing
+                    fontSizeValue = nextFontSizeValue
+                }
+            } else {
+
+                // Text fits before reaching the minimum, it's readyToDraw
+                readyToDraw = true
+
+            }
+        },
+        modifier = modifier.drawWithContent { if (readyToDraw) drawContent() }
+    )
+
+    Text(fontSizeValue.toString())
+}
+
+data class FontSizeRange(
+    val min: TextUnit,
+    val max: TextUnit,
+    val step: TextUnit = DEFAULT_TEXT_STEP,
+) {
+    init {
+        require(min < max) { "min should be less than max, $this" }
+        require(step.value > 0) { "step should be greater than 0, $this" }
+    }
+
+    companion object {
+        private val DEFAULT_TEXT_STEP = 1.sp
+    }
 }
