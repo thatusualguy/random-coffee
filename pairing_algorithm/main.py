3bc4102dd24cdaedbd45f4a4bd8282f1391034c2
NIKFIRE4 <nikitakostrov96@gmail.com>
2024-12-06 23:41:06 +0300

    Сохранение изменений перед слиянием

--- pairing_algorithm/main.py
+++ pairing_algorithm/main.py
@@ -76,4 +76,6 @@ async def get_recommendations(user_id: int):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
